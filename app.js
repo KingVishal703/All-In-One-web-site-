8df35293582b608ceff541df143645d05f3b4e6d
KingVishal703 <vishal21wankhede@gmail.com>
2026-01-30 13:08:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 let DB=[];
 let current=[];
+let unlocked=false;
 
 fetch("data.json")
 .then(r=>r.json())
 .then(d=>{
 DB=d;
 load("anime",document.querySelector(".tabs button"));
-setTimeout(()=>loader.style.display="none",800);
+setTimeout(showPopup,5000);
 });
 
 function load(type,btn){
@@ -19,25 +20,59 @@ render(current);
 
 function render(list){
 let html="";
-list.forEach(item=>{
+list.forEach((item,i)=>{
 html+=`
-<div class="card">
+<div class="card" onclick="openDetails(${i})">
 <img src="${item.image}">
 <h3>${item.title}</h3>
-<div class="button" onclick="verify('${encodeURIComponent(item.token)}')">
-Verify & Unlock
-</div>
 </div>`;
 });
 grid.innerHTML=html;
 }
 
-function searchContent(){
-let q=search.value.toLowerCase();
-let f=current.filter(x=>x.title.toLowerCase().includes(q));
-render(f);
+function openDetails(i){
+let d=current[i];
+
+details.innerHTML=`
+<div class="modal-box">
+<img src="${d.image}">
+<h2>${d.title}</h2>
+
+<p>🌐 Season - ${d.season}</p>
+<p>📀 Total Episode - ${d.episodes}</p>
+<p>⭐ Quality - ${d.quality}</p>
+<p>🎧 Language - ${d.language}</p>
+<p>⚡ Genre - ${d.genre}</p>
+
+<div class="button" onclick="verify('${encodeURIComponent(d.token)}')">
+Watch Now
+</div>
+
+<div class="cancel" onclick="closeDetails()">Cancel</div>
+</div>
+`;
+
+details.style.display="flex";
+}
+
+function closeDetails(){
+details.style.display="none";
 }
 
 function verify(link){
 window.location="verify.html?link="+link;
+}
+
+function showPopup(){
+if(unlocked) return;
+
+popup.style.display="flex";
+}
+
+function watchAd(){
+popup.innerHTML="<h2>Watching Ad...</h2>";
+setTimeout(()=>{
+popup.style.display="none";
+unlocked=true;
+},5000);
 }
