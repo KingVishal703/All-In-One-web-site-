8c6559d932b51bb99d40f1521462ffe5590da591
KingVishal703 <vishal21wankhede@gmail.com>
2026-01-30 18:29:25 +0530

    Update ads.js

--- ads.js
+++ ads.js
@@ -7,7 +7,7 @@ autoInterval:600000 // 10 min
 
 /* ===== PASTE YOUR BLOCK IDS HERE ===== */
 const IDS = {
-home:"int-18636",      // int-XXXXX
+home:"",      // int-XXXXX
 verify:"",    // int-XXXXX
 reward:""     // XXXXX
 };
