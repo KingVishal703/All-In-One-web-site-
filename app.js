377bdd8eee993a5574d7e0d0dd214758d62bc500
KingVishal703 <vishal21wankhede@gmail.com>
2026-01-30 17:43:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,20 +14,9 @@ fetch("data.json")
 .then(d=>{
 DB=d;
 loadCategory("anime",document.querySelector(".tabs button"));
-startAutoAds();
-checkDeepLink();
+runHomeAd();
+startAutoHomeAds();
 });
-
-function checkDeepLink(){
-let params=new URLSearchParams(location.search);
-let id=params.get("id");
-if(!id) return;
-let index=DB.findIndex(x=>x.id==id);
-if(index!==-1){
-current=[DB[index]];
-openDetails(0);
-}
-}
 
 function loadCategory(cat,btn){
 activeCategory=cat;
@@ -78,27 +67,20 @@ grid.innerHTML=html;
 
 function openDetails(i){
 let d=current[i];
-history.pushState({modal:true},"");
 
 details.innerHTML=`
 <div class="modal-box">
 
 <div class="topbar">
 <div class="topbtn" onclick="closeDetails()">← Back</div>
-<div class="topbtn" onclick="shareItem('${d.id}')">Share</div>
 </div>
 
 <img src="${d.image}">
 <h2 style="padding:10px">${d.title}</h2>
 
-<p style="padding:0 10px">🌐 Season - ${d.season}</p>
-<p style="padding:0 10px">📀 Total Episode - ${d.episodes}</p>
-<p style="padding:0 10px">⭐ Quality - ${d.quality}</p>
-<p style="padding:0 10px">🎧 Language - ${d.language}</p>
-<p style="padding:0 10px">⚡ Genre - ${d.genre}</p>
+<p style="padding:0 10px">Season: ${d.season}</p>
 
 <div class="button" onclick="verify('${encodeURIComponent(d.token)}')">Watch Now</div>
-<div class="cancel" onclick="closeDetails()">Back</div>
 
 </div>
 `;
@@ -108,35 +90,8 @@ details.style.display="block";
 
 function closeDetails(){
 details.style.display="none";
-history.back();
-}
-
-function shareItem(id){
-let link=location.origin+location.pathname+"?id="+id;
-navigator.clipboard.writeText(link);
-showToast("Share link copied!");
-}
-
-function showToast(msg){
-toast.innerText=msg;
-toast.style.display="block";
-setTimeout(()=>toast.style.display="none",2000);
 }
 
 function verify(link){
-runVerifyInterstitial();
 window.location="verify.html?link="+link;
 }
-
-function searchContent(){
-let q=search.value.toLowerCase();
-let f=DB.filter(x=>x.title.toLowerCase().includes(q));
-current=f;
-render(f);
-}
-
-window.onpopstate=function(){
-if(details.style.display==="block"){
-details.style.display="none";
-}
-};
