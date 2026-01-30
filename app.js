1d5cdac5664515e7f2a9a726787d1b0b8e61c366
KingVishal703 <vishal21wankhede@gmail.com>
2026-01-30 12:31:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-let DB = [];
-let current = [];
+let DB=[];
+let current=[];
 
 fetch("data.json")
-.then(res=>res.json())
-.then(data=>{
-  DB=data;
+.then(r=>r.json())
+.then(d=>{
+  DB=d;
   load("anime");
 });
 
 function load(type){
-current = DB.filter(x=>x.category===type);
+current=DB.filter(x=>x.category===type);
 render(current);
 }
 
@@ -20,20 +20,20 @@ html+=`
 <div class="card">
 <img src="${item.image}">
 <h3>${item.title}</h3>
-<div class="button" onclick="verify('${item.token}')">
+<div class="button" onclick="verify('${encodeURIComponent(item.token)}')">
 Verify & Unlock
 </div>
 </div>`;
 });
-document.getElementById("grid").innerHTML=html;
+grid.innerHTML=html;
 }
 
-function verify(token){
-window.location="verify.html?token="+token;
+function verify(link){
+window.location="verify.html?link="+link;
 }
 
 function searchContent(){
-let q=document.getElementById("search").value.toLowerCase();
-let filtered=current.filter(x=>x.title.toLowerCase().includes(q));
-render(filtered);
+let q=search.value.toLowerCase();
+let f=current.filter(x=>x.title.toLowerCase().includes(q));
+render(f);
 }
