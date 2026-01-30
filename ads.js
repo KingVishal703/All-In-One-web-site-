4ae622a3a5609650d4d04e5209048ac6a1245604
KingVishal703 <vishal21wankhede@gmail.com>
2026-01-30 20:26:13 +0530

    Update ads.js

--- ads.js
+++ ads.js
@@ -2,10 +2,9 @@ const ADS = {
 homeInterstitial:true,
 verifyInterstitial:true,
 reward:true,
-autoInterval:600000 // 10 min
+autoInterval:600000
 };
 
-/* ===== YOUR BLOCK IDS ===== */
 const IDS = {
 home:"int-22046",
 verify:"int-22056",
@@ -16,31 +15,14 @@ let homeAd=null;
 let verifyAd=null;
 let rewardAd=null;
 
-/* ============================= */
-/* WAIT UNTIL ADSGRAM LOADS */
-/* ============================= */
-
 function waitForAdsgram(callback){
-let tries=0;
-
 let timer=setInterval(()=>{
-tries++;
-
 if(window.Adsgram){
 clearInterval(timer);
 callback();
 }
-
-if(tries>20){
-clearInterval(timer);
-console.log("Adsgram failed to load");
-}
 },300);
 }
-
-/* ============================= */
-/* INIT ADS SAFELY */
-/* ============================= */
 
 function initAds(){
 
@@ -57,64 +39,28 @@ homeAd = safe(IDS.home);
 verifyAd = safe(IDS.verify);
 rewardAd = safe(IDS.reward);
 
-console.log("Ads initialized");
+console.log("Ads ready");
 }
 
-/* start waiting */
 waitForAdsgram(initAds);
 
-/* ============================= */
-/* HOME INTERSTITIAL */
-/* ============================= */
-
 function runHomeAd(){
-if(!ADS.homeInterstitial || !homeAd) return;
-
-homeAd.show()
-.then(()=>console.log("Home ad done"))
-.catch(()=>console.log("Home ad error"));
+if(homeAd) homeAd.show().catch(()=>{});
 }
-
-/* ============================= */
-/* VERIFY INTERSTITIAL */
-/* ============================= */
 
 function runVerifyAd(){
-if(!ADS.verifyInterstitial || !verifyAd) return;
-
-verifyAd.show()
-.then(()=>console.log("Verify ad done"))
-.catch(()=>console.log("Verify ad error"));
+if(verifyAd) verifyAd.show().catch(()=>{});
 }
-
-/* ============================= */
-/* REWARD AD */
-/* ============================= */
 
 function runReward(callback){
-if(!ADS.reward || !rewardAd){
-callback();
-return;
-}
+if(!rewardAd){callback();return;}
 
 rewardAd.show()
-.then(()=>{
-console.log("Reward success");
-callback();
-})
-.catch(()=>{
-console.log("Reward error");
-callback();
-});
+.then(()=>callback())
+.catch(()=>callback());
 }
 
-/* ============================= */
-/* AUTO LOOP */
-/* ============================= */
-
 function startAutoHomeAds(){
-if(!ADS.homeInterstitial) return;
-
 setInterval(()=>{
 runHomeAd();
 },ADS.autoInterval);
